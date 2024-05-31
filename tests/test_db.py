9cd509b37ea08b56b0166eb562244b79de3d637e
marcus-wrrn <marcuswrrn@gmail.com>
2024-05-31 09:41:21 -0400

    feat: added unit test for db

---
+++ tests/test_db.py
@@ -0,0 +1,34 @@
+import unittest
+import sys
+import os
+import numpy as np
+
+# Adjust the path to ensure the src module can be imported
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from src.database.orm import Database
+import env
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+class TestDatabase(unittest.TestCase):
+    def setUp(self):
+        # Yes I'm using the same db path, should not be an issue as a new db can be generated rapidly
+        self.db = Database(env.DB_PATH)
+    
+    def tearDown(self):
+        self.db.conn.close()
+    
+    def test_vocab_size(self):
+        vocab_path = env.VOCAB_EMB_PATH
+
+        vocab_emb = np.load(vocab_path)
+        vocab_size = vocab_emb.shape[0]
+
+        pruned_vocab_words = self.db.get_pruned_vocab()
+        pruned_size = len(pruned_vocab_words)
+
+        self.assertEqual(vocab_size, pruned_size)
+
+if __name__ == "__main__":
+    unittest.main()
